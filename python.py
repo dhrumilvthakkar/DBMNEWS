d87d6a2e3fd4376608df71131148f88de56ecb59
dhrumilvthakkar <dhrumil.t2025d@iimbg.ac.in>
2025-02-07 22:40:43 -0500

    prime number

---
+++ python.py
@@ -0,0 +1,21 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def largest_prime_below(limit):
+    for num in range(limit - 1, 1, -1):
+        if is_prime(num):
+            return num
+
+limit = 100000
+print(f"The largest prime number below {limit} is {largest_prime_below(limit)}")
